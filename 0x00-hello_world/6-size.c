a1429ad8d752dd310bbacdd27de602413a5b89e5
Naeet <nathnaelmeonnen@gmail.com>
2023-10-05 07:00:15 -0700

    -m

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *  main - Entry point
+ * main - Entry point
  * 
  * Return: Always 0 (Success)
  */
@@ -15,5 +15,3 @@ int main(void)
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
-
-
