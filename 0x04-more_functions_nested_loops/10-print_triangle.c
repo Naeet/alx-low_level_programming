6390765c1da2c292cf631072c1e5709a3d16807e
Naeet <nathnaelmeonnen@gmail.com>
2023-10-12 07:15:25 -0700

    -m

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,22 +8,28 @@
 
 void print_triangle(int size)
 {
-	int i, j;
+	int c, i, j;
+
+	c = 0;
+	i = size - 1;
+	while (c < size)
+	{
+		i = size - 1 - c;
+		j = c + 1;
+		while (i > 0)
+		{
+			_putchar(' ');
+			i--;
+		}
+		while (j > 0)
+		{
+			_putchar('#');
+			j--;
+		}
+		_putchar('\n');
+		c++;
+	}
 
 	if (size <= 0)
 		_putchar('\n');
-	else
-	{
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
-		{
-			if (j >= i)
-				_putchar('#');
-			else
-				_putchar(' ');
-		}
-		_putchar('\n');
-	}
-	}
 }
