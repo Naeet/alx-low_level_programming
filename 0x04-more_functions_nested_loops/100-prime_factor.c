75e5b8a64a82435db316492f0744d3a72c56597c
Naeet <nathnaelmeonnen@gmail.com>
2023-10-12 07:23:38 -0700

    -m

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - main fun
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	long num = 612852475143;
+
+	for (i = (int) sqrt(num); i > 2 ; i++)
+	{
+		if (num % i == 0)
+		{
+			printf("%d\n", i);
+			break;
+		}
+	}
+	return (0);
+}
