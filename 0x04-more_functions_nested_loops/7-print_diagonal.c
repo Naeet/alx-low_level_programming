d3f5c5b2e4339a7b642783da37eff77c46a6393e
Naeet <nathnaelmeonnen@gmail.com>
2023-10-12 06:33:36 -0700

    -m

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,12 +15,12 @@ void print_diagonal(int n)
 		_putchar('\n');
 	else
 	{
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j <= i; j++)
-			_putchar(' ');
-		_putchar('\\');
-		_putchar('\n');
-	}
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j <= i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
 	}
 }
