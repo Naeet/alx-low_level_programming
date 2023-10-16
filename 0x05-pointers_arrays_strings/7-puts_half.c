ae212c6f1dd62dd2f7226f9fd78338c9ed1f282f
Naeet <nathnaelmeonnen@gmail.com>
2023-10-16 07:27:43 -0700

    main.h

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts_half - print last half of string
+ * @str: parameter
+ */
+void puts_half(char *str)
+{
+	int i, count = 0, n;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+		count++;
+	n = (count - 1) / 2;
+
+	for (i = n + 1 ; str[i] != '\0' ; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
