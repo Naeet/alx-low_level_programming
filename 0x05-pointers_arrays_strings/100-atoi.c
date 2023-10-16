2be36d1164bc8dbbc1f9009d3070215cea8cd83d
Naeet <nathnaelmeonnen@gmail.com>
2023-10-16 08:08:05 -0700

    main.h

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,27 +10,19 @@
 int _atoi(char *s)
 {
 	int sign = 1;
-	int result = 0;
-	int i = 0;
+	unsigned int num = 0;
 
-	if (s[i] == '-')
-	{
-		sign = -1;
-		i++;
-	}
+	do {
+		if (*s == '-')
+			sign *= -1;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result = result * 10 + (s[i] - '0');
-		}
-		else if (result > 0)
-		{
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
 			break;
-		}
-		i++;
-	}
 
-	return (result * sign);
+	} while (*s++);
+
+	return (num * sign);
 }
