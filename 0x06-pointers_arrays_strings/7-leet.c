08c8f9be46cc2f6dedbc9935647058a03058317c
Naeet <nathnaelmeonnen@gmail.com>
2023-10-19 01:16:30 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,7 +12,7 @@ char *leet(char *s)
 	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
 
-	for (i = 0 ; s[i] != 0 ; s++)
+	for (i = 0 ; s[i] != ('\0') ; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
