fcc14185e6ececfe82d459a61ac6cfa5f187a015
Naeet <nathnaelmeonnen@gmail.com>
2023-10-19 04:52:12 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - entry fun
+ *
+ * Return: always 0
+ */
 
 int main(void)
 {
@@ -19,5 +24,5 @@ int main(void)
 	  *(p + 5) = 98;
 	  /* ...so that this prints 98\n */
 	  printf("a[2] = %d\n", a[2]);
-	  return (0);
+	return (0);
 }
