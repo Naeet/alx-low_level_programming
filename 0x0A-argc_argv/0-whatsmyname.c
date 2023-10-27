8700e3fa1d95163d6e27c55fff8b4e16171bc02e
Naeet <nathnaelmeonnen@gmail.com>
2023-10-27 03:54:43 -0700

    -m

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - fun printing name of fun
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: always 0
+ *
+ */
+int main(int argc,char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
