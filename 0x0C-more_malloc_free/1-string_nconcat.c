90daf5e9a527dba6c4042fde8ef0aa4e17f1a51c
Naeet <nathnaelmeonnen@gmail.com>
2023-11-02 03:33:46 -0700

    -m

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,11 +27,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (n >= s2len)
 		n = s2len;
+	else
+		n = n;
 
 	for (i = 0; s1[i] != '\0'; i++)
 		output[i] = s1[i];
 	for (i = 0; i < n; i++)
 		output[s1len + i] = s2[i];
+	output[s1len + i] = '\0';
 
 	return (output);
 }
