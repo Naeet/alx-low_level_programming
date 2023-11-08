81f3db8e5f380f8cccf3d2ab9890999b760b0eeb
Naeet <nathnaelmeonnen@gmail.com>
2023-11-08 06:10:15 -0800

    -m

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -6,7 +6,7 @@
  * main - entry
  * @argc: arg
  * @argv: arg
- * Return : always 0
+ * Return: always 0
  */
 int main(int argc, char *argv[])
 {
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -7,7 +7,7 @@
  */
 int op_add(int a, int b)
 {
-	return (a + b);;
+	return (a + b);
 }
 /**
  * op_sub - subtracts two numbers
