67cc657e8a705f8abe989ca0b472bba88a0c7f61
Naeet <nathnaelmeonnen@gmail.com>
2023-11-08 05:46:56 -0800

    -m

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "FUNCTION_POINTERS.H"
+#include <stdio.h>
+/**
+ * int_index -  fun that searches for an array
+ * @array: array
+ * @size: size
+ * @cmp: fun
+ * Return: int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) == 1)
+			return (i);
+	}
+	return (-1);
+}
