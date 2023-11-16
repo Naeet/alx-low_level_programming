cb6ece2d1ce5d7dfce5cd9c7cdb42102b6095b8e
Naeet <nathnaelmeonnen@gmail.com>
2023-11-16 05:23:20 -0800

    -m

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * add_node - add node
+ * @head: head
+ * @str: str to be added
+ *
+ * Return: address of new elt
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newnode;
+	unsigned int i, count = 0;
+
+	newnode = malloc(sizeof(list_t));
+
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->str = strdup(str);
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+	newnode->len = count;
+	newnode->next = *head;
+	*head = newnode;
+
+	return (*head);
+}
