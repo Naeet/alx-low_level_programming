8df954cf6b2ccc6c99a404e6cf9fe8025079883e
Naeet <nathnaelmeonnen@gmail.com>
2023-11-16 05:47:43 -0800

    -m

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,17 @@
+extern printf
 section .data
-	hello_msg db 'Hello, Holberton', 0
-	format db '%s', 0
+	msg: db "Hello, Holberton", 0
+	fmt: db "%s", 10, 0
 
 section .text
 	global main
-	extern printf
-
 main:
-	; Push arguments onto the stack in reverse order
-	push hello_msg
-	push format
-
-	; Call printf function
+	push rbp
+	mov rdi,fmt
+	mov rsi,msg
+	mov rax,0
 	call printf
 
-	; Clean up the stack
-	add rsp, 16
-
-	; Exit the program
-	mov eax, 0
+	pop rbp
+	mov rax,0
 	ret
