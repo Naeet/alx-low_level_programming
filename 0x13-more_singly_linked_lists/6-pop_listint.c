58f91c8dcb66c3237b5c1948be817a37be99d6a7
Naeet <nathnaelmeonnen@gmail.com>
2023-11-21 00:48:15 -0800

    -m

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * pop_listint - del head node and
+ * @head: head of node
+ * Return: head node's data
+ */
+int pop_listint(listint_t **head)
+{
+	int i;
+	listint_t *current;
+	
+	if (head == NULL)
+		return (0);
+	current = *head;
+	if(*head)
+	{
+		i = current->n;
+		*head = current->next;
+		free(current);
+	}
+	else
+		i = 0;
+	return (i);
+}
