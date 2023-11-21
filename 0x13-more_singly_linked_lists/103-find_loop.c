b43f08265da66d0f7581ce64133d9fdbdde5f7c0
Naeet <nathnaelmeonnen@gmail.com>
2023-11-21 07:28:00 -0800

    -m

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * find_listint_loop - fun
+ * @head: head
+ * Return: ptr
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *tortoise = head;
+	listint_t *hare = head;
+
+	while (tortoise != NULL && hare != NULL && hare->next != NULL)
+	{
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			return (tortoise);
+		}
+	}
+	return (NULL);
+}
