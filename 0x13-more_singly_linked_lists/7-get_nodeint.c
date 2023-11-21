e46c216f0ccc1a67111acff228e716f89b72e3d2
Naeet <nathnaelmeonnen@gmail.com>
2023-11-21 02:47:57 -0800

    -m

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,7 +2,7 @@
 /**
  * get_nodeint_at_index - returns nth node
  * @head: head of node
- * @n: nth int to return the data
+ * @index: nth int to return the data
  * Return: nth node
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
